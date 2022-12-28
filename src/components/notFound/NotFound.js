3e45c7b31f24e978a3d6a9b7d162560285da1876
najmul-eimon <najmul.eimon@intelli.global>
2022-12-28 11:59:54 +0600

    not found page update

--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
-import image from '../../assets/images/not-found1.png';
+import image from '../../assets/images/not-found.png';
 import '../../assets/css/not-found.css';
 
 const NotFound = () => {
 
   return (
     <main>
-      <section className='not-found'>
+      <section className="not-found">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <img src={image} alt="" />
               <div className="d-flex align-items-center justify-content-center">
-                <Link to="/">Back to Home</Link>
+                <Link to="/" className='btn-style'>Back to Home</Link>
               </div>
             </div>
           </div>
